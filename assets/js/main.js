e2ebef3d9fd0e93b45d3711251509260f0cc54a2
unknown <fabioflauzino@hotmail.com>
2022-06-04 00:07:16 -0300

    finalizando mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,38 @@
-const navigation = document.querySelector('#navigation')
-const menuOpen = document.querySelector('.open-menu')
-const menuClose = document.querySelector('.close-menu')
+const navigation = document.querySelector("#navigation");
+const menuOpen = document.querySelector(".open-menu");
+const menuClose = document.querySelector(".close-menu");
+const links = document.querySelectorAll(".menu a");
 
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
+  if (scrollY > 0) {
+    navigation.classList.add("scroll");
+  } else {
+    navigation.classList.remove("scroll");
+  }
+});
 
-    if (scrollY > 0) {
-        navigation.classList.add('scroll')
-    } else {
-        navigation.classList.remove('scroll')
-    }
-
-})
-
-if(menuOpen) {
-    menuOpen.addEventListener('click', () => {
-        document.body.classList.add('menu-expanded')
-    })
+if (menuOpen) {
+  menuOpen.addEventListener("click", () => {
+    document.body.classList.add("menu-expanded");
+  });
 }
 
-if(menuClose) {
-    menuClose.addEventListener('click', () => {
-        document.body.classList.remove('menu-expanded')
-    })
+if (menuClose) {
+  menuClose.addEventListener("click", () => {
+    document.body.classList.remove("menu-expanded");
+  });
 }
 
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.body.classList.remove("menu-expanded");
+  });
+});
+
+ScrollReveal({
+  origin: "top",
+  distance: "50px",
+  duration: 700,
+}).reveal(`#home, #home img, #home .stats, 
+            #services
+        `);
