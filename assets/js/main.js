eb97088eb189efa56bf3482e9e829f9f5d3c6e11
unknown <fabioflauzino@hotmail.com>
2022-06-05 10:35:17 -0300

    finalizando projeto com html css

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,48 @@ const menuOpen = document.querySelector(".open-menu");
 const menuClose = document.querySelector(".close-menu");
 const links = document.querySelectorAll(".menu a");
 
+
+
 window.addEventListener("scroll", () => {
+  //mostrandto menu
   if (scrollY > 0) {
     navigation.classList.add("scroll");
   } else {
     navigation.classList.remove("scroll");
   }
+
+  ativarMenuSessaoCorrente(home);
+  ativarMenuSessaoCorrente(services);
+  ativarMenuSessaoCorrente(about);
+  ativarMenuSessaoCorrente(contact);
+
 });
+
+function ativarMenuSessaoCorrente(section) {
+
+  const targetLine = scrollY + innerHeight / 2
+
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+  const passedTargetLine = targetLine >= sectionTop
+
+
+  const finalSection = sectionTop + sectionHeight
+  const finalSectionPassedTarget = finalSection <= targetLine
+
+  const sectionLimits = passedTargetLine && !finalSectionPassedTarget
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+
+  if(sectionLimits){
+    menuElement.classList.add('active')
+  }
+
+
+}
 
 if (menuOpen) {
   menuOpen.addEventListener("click", () => {
